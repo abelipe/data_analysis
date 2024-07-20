d3eee13a5a935dd744876e2a18cb7ca84118f8c2
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-20 00:13:39 -0400

    Start of data analysis

---
+++ analysis.py
@@ -0,0 +1,29 @@
+# Analysis of wind data
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    pass
+
+def plot_scatter_first_1000():
+    pass
+
+def plot_scatter_greatest_speed_1000():
+    pass
+
+def plot_histogram_speed():
+    pass
+
+def monthly_mean_speed():
+    pass
+
+def table_from_historic():
+    pass
+
+def plot_monthly_historic():
+    pass
+
+
+if __name__ = "__main__":
+    main()
+
